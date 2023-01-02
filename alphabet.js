0e0bd1ec47ccca646a753253284b9368f453724c
Donovan Martinez <88170488+donovanMZ@users.noreply.github.com>
2023-01-01 22:15:14 -0800

    Add Initial Files
    A basic website with flexbox for the body, container, and letter objects. Basic hover transitions and javascript onClick alert.

---
+++ alphabet.js
@@ -0,0 +1,130 @@
+function a () {
+    alert("I am an A");
+    console.log("I am an A 'alert' function");
+}
+
+
+function b () {
+    alert("I am an B");
+    console.log("I am an B 'alert' function");
+}
+
+function c () {
+    alert("I am an C");
+    console.log("I am an C 'alert' function");
+}
+
+function d () {
+    alert("I am an D");
+    console.log("I am an D 'alert' function");
+}
+
+function e () {
+    alert("I am an E");
+    console.log("I am an E 'alert' function");
+}
+
+function f () {
+    alert("I am an F");
+    console.log("I am an F 'alert' function");
+}
+
+function g () {
+    alert("I am an G");
+    console.log("I am an G 'alert' function");
+}
+
+function h () {
+    alert("I am an H");
+    console.log("I am an H 'alert' function");
+}
+
+function i () {
+    alert("I am an I");
+    console.log("I am an I 'alert' function");
+}
+
+function j () {
+    alert("I am an J");
+    console.log("I am an J 'alert' function");
+}
+
+function k () {
+    alert("I am an K");
+    console.log("I am an K 'alert' function");
+}
+
+function l () {
+    alert("I am an L");
+    console.log("I am an L 'alert' function");
+}
+
+function m () {
+    alert("I am an M");
+    console.log("I am an M 'alert' function");
+}
+
+function n () {
+    alert("I am an N"); 
+    console.log("I am an N 'alert' function");
+}
+
+function o () {
+    alert("I am an O");
+    console.log("I am an O 'alert' function");
+}
+
+function p () {
+    alert("I am an P");
+    console.log("I am an P 'alert' function");
+}
+
+function q () {
+    alert("I am an Q");
+    console.log("I am an Q 'alert' function");
+}
+
+function r () {
+    alert("I am an R");
+    console.log("I am an R 'alert' function");
+}
+
+function s () {
+    alert("I am an S");
+    console.log("I am an S 'alert' function");
+}
+
+function t () {
+    alert("I am an T");
+    console.log("I am an T 'alert' function");
+}
+
+function u () {
+    alert("I am an U");
+    console.log("I am an U 'alert' function");
+}
+
+function v () {
+    alert("I am an V");
+    console.log("I am an V 'alert' function");
+}
+
+function w () {
+    alert("I am an W");
+    console.log("I am an W 'alert' function");
+}
+
+function x () {
+    alert("I am an X");
+    console.log("I am an X 'alert' function");
+}
+
+function y () {
+    alert("I am an Y");
+    console.log("I am an Y 'alert' function");
+}
+
+function z () {
+    alert("I am an Z");
+    console.log("I am an Z 'alert' function");
+}
